84a6d2fc555b9f336f09ddcdf68a94ed7538c6ab
vsaran93 <vishnusaran37@gmail.com>
2023-11-25 01:24:22 +0800

    added dashboard route and layout

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Dashboard() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div>
+          <h1>Hello to Dashboard</h1>
+        </div>
+      </main>
+    )
+  }
+  
